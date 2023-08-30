1146be44e04a9762c532725adb051b66e0183cb8
My-Java_DSA <pratiksutar841@gmail.com>
2023-08-30 18:59:23 +0530

    Sum ofEven index

---
+++ Array14.java
@@ -0,0 +1,15 @@
+//1. WAP to print the sum of all the elements present on even indexes in the given array.
+public class Array14 
+{         
+         public static void main(String[] args) 
+         {
+              int[] arr = {1,2,3,4,5,6,7,8};
+              int i = 0, sum = 0;
+                 while (i < arr.length) 
+                 {
+                    sum += arr[i];
+                    i += 2;
+                 }
+                     System.out.println(sum);
+          }               
+}
